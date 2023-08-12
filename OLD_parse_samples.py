a1505a6b11dc430b3f636e9be6ef9a9803179164
Jensen Richardson <jensen.richardson@utexas.edu>
2023-08-12 13:34:42 -0500

    Small refactoring and added nextflow option to ps

--- OLD_parse_samples.py
+++ OLD_parse_samples.py
@@ -27,8 +27,7 @@ class Sample:
         return rep
 
     def addReadgroup(self, rg):
-        self.readgroups.append(
-            rg) if rg not in self.readgroups else self.readgroups
+        self.readgroups.append(rg) if rg not in self.readgroups else self.readgroups
 
 
 class Readgroup:
@@ -115,20 +114,19 @@ def main(args):
     if args.star:
         for s in samples:
             s.command = getStarCommand(s)
-    if args.bwa or args.nextflow:
+    if args.bwa2:
         for s in samples:
             s = getBwaCommand(s)
     # Constructs a dictionary with the following format:
     # "sample": ["sample name", ["rg1"...], "star command"]
-    sample_dict = constructDict(samples, args.star, args.bwa, args.nextflow)
+    sample_dict = constructDict(samples, args.star, args.bwa2)
     if args.output:
-        directory = directory.parent
-        tsv_file = os.path.join(directory, args.output)
+        tsv_file = args.output
     else:
         directory = directory.parent
         tsv_file = os.path.join(directory, "samples.tsv")
-    print_tsv(sample_dict, tsv_file, args.star, args.bwa, args.nextflow)
-    print(f'Formed {tsv_file} of {len(samples)} samples.')
+    print_tsv(sample_dict, tsv_file, args.star, args.bwa2)
+    print(f'Formed {tsv_file}/samples.tsv of {len(samples)} samples.')
 
 
 def get_samples(fastq_files):
@@ -142,8 +140,7 @@ def get_samples(fastq_files):
         # Makes a regex looking for that sample name
         name_match = re.compile(f"^{sample.name}")
         # Adds that sample to the list of samples if there isn't already one with that name initialized
-        samples if any(name_match.match(s.name)
-                       for s in samples) else samples.append(sample)
+        samples if any(name_match.match(s.name) for s in samples) else samples.append(sample)
     # sorts the list of samples alphabetically (had to use a lambda function because I used Sample Class)
     samples.sort(key=lambda s: s.name)
     return samples
@@ -177,8 +174,7 @@ def get_readgroups(samples, fastq_files, fastq_dir, verb):
                 r2 = glob.glob(f'{os.path.join(fastq_dir, rg)}*R2*')
                 if not r1 or not r2:
                     print(r1, r2)
-                    raise Exception(
-                        f'Either R1: {r1} or R2: {r2} was not caught. File bug report')
+                    raise Exception(f'Either R1: {r1} or R2: {r2} was not caught. File bug report')
                 # Creates readgroup object
                 final_rg = Readgroup()
                 final_rg.rg = rg
@@ -217,10 +213,8 @@ def getBwaCommand(sample):
     return sample
 
 
-def constructDict(samples, star, bwa, nextflow):
+def constructDict(samples, star, bwa):
     sample_dict = {}
-    if nextflow:
-        sample_dict["nextflow_table"] = []
     for s in samples:
         files = [rg.r1 for rg in s.readgroups] + [rg.r2 for rg in s.readgroups]
         if star:
@@ -233,49 +227,33 @@ def constructDict(samples, star, bwa, nextflow):
                 file2 = readgroup.r2
                 command = readgroup.command
                 groups[name] = [file1, file2, command]
-            sample_dict[s.name] = [s.name, groups]
-        elif nextflow:
-            for rg in s.readgroups:
-                sample_dict["nextflow_table"].append(
-                    [s.name, rg.rg, rg.r1, rg.r2, rg.command])
+            sample_dict[s.name] = [s.name, groups, s.command]
         else:
             sample_dict[s.name] = [s.name, files]
     return sample_dict
 
 
-def print_tsv(sample_d, tsv_file, star, bwa, nextflow):
+def print_tsv(sample_d, tsv_file, star, bwa):
     if star:
-        data = pd.DataFrame.from_dict(sample_d, orient='index', columns=[
-                                      'sample_name', 'files', 'command'])
+        data = pd.DataFrame.from_dict(sample_d, orient='index', columns=['sample_name', 'files', 'command'])
     elif bwa:
-        data = pd.DataFrame.from_dict(sample_d, orient='index', columns=[
-                                      'sample_name', 'files'])
-    elif nextflow:
-        data = pd.DataFrame(sample_d["nextflow_table"], columns=[
-                            'sample_name', 'readgroup', 'r1', 'r2', 'bwa_read_group_string'])
+        data = pd.DataFrame.from_dict(sample_d, orient='index', columns=['sample_name', 'files', 'command'])
     else:
-        data = pd.DataFrame.from_dict(sample_d, orient='index', columns=[
-                                      'sample_name', 'files'])
+        data = pd.DataFrame.from_dict(sample_d, orient='index', columns=['sample_name', 'files'])
     data.to_csv(tsv_file, sep='\t', index=False)
 
 
 if __name__ == "__main__":
     # Parses arugments
     # Only argument is for the directory
-    parser = argparse.ArgumentParser(
-        description="Create sample tsv for RNAseq mapping with STAR")
-    parser.add_argument("fastq_dir", type=dir_path,
-                        help="Directory containing fastq files")
+    parser = argparse.ArgumentParser(description="Create sample tsv for RNAseq mapping with STAR")
+    parser.add_argument("fastq_dir", type=dir_path, help="Directory containing fastq files")
     parser.add_argument("-o", "--output", type=str, help="Name of output tsv")
-    parser.add_argument("-v", "--verbose", action="count",
-                        default=0, help="Enable debug output")
-    parser.add_argument("-s", "--star", action="store_true",
-                        help="Form commands for mapping with STAR.")
-    parser.add_argument("-b", "--bwa", action="store_true",
-                        help="Form commands for mapping with bwa (mem 2).")
-    parser.add_argument("-n", "--nextflow", action="store_true",
-                        help="Format for nextflow. Creates table with sample_name, readgroup, r1, r2, bwa_read_group_string as a tsv.")
+    parser.add_argument("-v", "--verbose", action="count", default=0, help="Enable debug output")
+    parser.add_argument("-s", "--star", action="store_true", help="Form commands for mapping with STAR.")
+    parser.add_argument("-b", "--bwa2", action="store_true", help="Form commands for mapping with bwa mem 2.")
     parser.add_argument("-i", "--input-samples", type=str, help="Provide a tsv of sample names to look for in the "
                                                                 "fastq directory.")
     args = parser.parse_args()
     main(args)
+    exit(0)
